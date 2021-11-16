518c0fc4d3e65e7f1eaa644ad3db62f24de484c9
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-11-16 23:13:56 +0900

    Update

--- python/VoiceChannelNotification.py
+++ python/VoiceChannelNotification.py
@@ -2,13 +2,13 @@ def VoiceChannelNotification(member, beforeChannel, afterChannel):
     returnValue = None
     if beforeChannel != afterChannel and afterChannel is not None and afterChannel.id in [777032856286396456]:
         if member.id in [413611857778180096]:
-            emoji = ":video_game:"
+            emoji = ":banana:"
         elif member.id in [731679198615437715]:
-            emoji = ":tools:"
+            emoji = ":robot:"
         elif member.id in [777105632342966273]:
-            emoji = ":underage:"
+            emoji = ":u7981:"
         elif member.id in [778607585586053127]:
-            emoji = ":dog:"
+            emoji = ":pig:"
         returnValue = emoji + " __" + member.name + "__ が **" + \
             afterChannel.name + "** に参加しました！" + emoji
     return returnValue
