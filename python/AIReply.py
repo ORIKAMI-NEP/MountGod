c1b68d7280e62aa4ab6eb13eb0c978cd28ba22f4
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-11-17 20:28:07 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -5,5 +5,5 @@ def AIReply(message):
     returnValue = None
     if "AI" in message:
         returnValue = requests.get(
-            "http://localhost:51401/?message="+message).json()["reply"]
+            "http://10.40.3.171:51401/?message="+message).json()["reply"]
     return returnValue
