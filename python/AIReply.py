6eed356e20abc901c84d71a7e93c53e363b812b3
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-12 18:43:24 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -1,9 +1,15 @@
+import subprocess
 import requests
 
 
 def AIReply(message):
     returnValue = None
     if "\\ai" in message:
-        returnValue = requests.get(
-            "http://10.40.3.171:51400/?message="+message).json()
+        try:
+            # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
+            subprocess.call("ssh orikami@202.231.44.104")
+            subprocess.call("nepgear325")
+            returnValue = subprocess.check_output("wget http://10.40.3.171:51400/?message="+message)
+        except Exception as e:
+            return "エラー：" + e.args[0]
     return returnValue
