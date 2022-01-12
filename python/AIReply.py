41d601ab17838cfed7d967f920170a68c320d2be
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-12 20:50:14 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -6,11 +6,11 @@ def AIReply(message):
     returnValue = None
     if "\\ai " in message:
         url = "http://10.40.3.171:51400/?message=" + message.replace("\\ai ", "")
-        try:
+        #try:
             # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
-            returnValue = subprocess.run(["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "curl", url])
-            print(returnValue[:str(returnValue).find(")")]+"\n")
-            returnValue=None
-        except Exception as e:
-            return "エラー：" + e
+        returnValue = subprocess.run(["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "curl", url])
+        print(returnValue[:returnValue.find(")")]+"\n")
+        returnValue=None
+        #except Exception as e:
+            #return "エラー：" + e
     return returnValue
