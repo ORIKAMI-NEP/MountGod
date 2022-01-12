9f8867610bbc5359dbcfb8434a3a5a9c1cfff819
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-12 21:01:36 +0900

    Update

--- python/Reminder.py
+++ python/Reminder.py
@@ -4,24 +4,25 @@ import datetime
 
 def SetReminder(message):
     returnValue = None
-    messageArray = message.split()
-    if "\\sr" == messageArray[0]:
-        returnValue = "引数の長さが違います"
-        if len(messageArray) == 7:
-            with open("json/ReminderData.json", "r", encoding="utf-8") as ReminderData:
-                ReminderData = json.load(ReminderData)
-                date = messageArray[1] + "/" + messageArray[2].zfill(2) + "/" + messageArray[3].zfill(
-                    2) + " " + messageArray[4].zfill(2) + ":" + messageArray[5].zfill(2)
-                for data in ReminderData:
-                    if date == data["date"]:
-                        return "その時間には既にリマインダーがセットされています"
-                ReminderData.append(
-                    dict({"date": date, "message": messageArray[6]}))
-            with open("json/ReminderData.json", "wb") as ReminderDataFile:
-                ReminderDataFile.write(json.dumps(
-                    ReminderData, ensure_ascii=False, indent=2, separators=(",", ": ")).encode("utf-8"))
-                ReminderDataFile.write("\n".encode())
-            returnValue = "リマインダーをセットしました"
+    if "\\sr" in message:
+        messageArray = message.split()
+        if "\\sr" == messageArray[0]:
+            returnValue = "引数の長さが違います"
+            if len(messageArray) == 7:
+                with open("json/ReminderData.json", "r", encoding="utf-8") as ReminderData:
+                    ReminderData = json.load(ReminderData)
+                    date = messageArray[1] + "/" + messageArray[2].zfill(2) + "/" + messageArray[3].zfill(
+                        2) + " " + messageArray[4].zfill(2) + ":" + messageArray[5].zfill(2)
+                    for data in ReminderData:
+                        if date == data["date"]:
+                            return "その時間には既にリマインダーがセットされています"
+                    ReminderData.append(
+                        dict({"date": date, "message": messageArray[6]}))
+                with open("json/ReminderData.json", "wb") as ReminderDataFile:
+                    ReminderDataFile.write(json.dumps(
+                        ReminderData, ensure_ascii=False, indent=2, separators=(",", ": ")).encode("utf-8"))
+                    ReminderDataFile.write("\n".encode())
+                returnValue = "リマインダーをセットしました"
     return returnValue
 
 
@@ -41,23 +42,24 @@ def GetReminder(message):
 
 def RemoveReminder(message):
     returnValue = None
-    messageArray = message.split()
-    if "\\rr" == messageArray[0]:
-        returnValue = "引数の長さが違います"
-        if len(messageArray) == 2:
-            with open("json/ReminderData.json", "r", encoding="utf-8") as ReminderData:
-                ReminderData = json.load(ReminderData)
-                if len(ReminderData) == 0:
-                    return "リマインダーが存在しません"
-                try:
-                    del ReminderData[int(messageArray[1])]
-                except Exception as e:
-                    return "エラー：" + e
-            with open("json/ReminderData.json", "wb") as ReminderDataFile:
-                ReminderDataFile.write(json.dumps(
-                    ReminderData, ensure_ascii=False, indent=2, separators=(",", ": ")).encode("utf-8"))
-                ReminderDataFile.write("\n".encode())
-            returnValue = "リマインダーを削除しました"
+    if "\\rr" in message:
+        messageArray = message.split()
+        if "\\rr" == messageArray[0]:
+            returnValue = "引数の長さが違います"
+            if len(messageArray) == 2:
+                with open("json/ReminderData.json", "r", encoding="utf-8") as ReminderData:
+                    ReminderData = json.load(ReminderData)
+                    if len(ReminderData) == 0:
+                        return "リマインダーが存在しません"
+                    try:
+                        del ReminderData[int(messageArray[1])]
+                    except Exception as e:
+                        return "エラー：" + e
+                with open("json/ReminderData.json", "wb") as ReminderDataFile:
+                    ReminderDataFile.write(json.dumps(
+                        ReminderData, ensure_ascii=False, indent=2, separators=(",", ": ")).encode("utf-8"))
+                    ReminderDataFile.write("\n".encode())
+                returnValue = "リマインダーを削除しました"
     return returnValue
 
 
