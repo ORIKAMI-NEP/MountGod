2d1fb1c928a6a8f1febf3b4cbe9b70ac8586ceb8
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-12 21:04:45 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -10,6 +10,8 @@ def AIReply(message):
             # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
         returnValue = subprocess.run(["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "curl", url])
         print(str(returnValue)[:str(returnValue).find(")")]+"\n")
+        print(str(returnValue)[:str(returnValue).find(")")]+"\n")
+        print(str(returnValue)[:str(returnValue).find(")")]+"\n")
         returnValue=None
         #except Exception as e:
             #return "エラー：" + e
