4d114a7f3cfacc87f90d31574109edb79cc4b415
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-12 20:38:51 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -5,11 +5,12 @@ import requests
 def AIReply(message):
     returnValue = None
     if "\\ai " in message:
-        message = message.replace("\\ai ", "")
+        url = "http://10.40.3.171:51400/?message=" + message.replace("\\ai ", "")
         try:
             # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
-            subprocess.run(["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104"])
-            returnValue = subprocess.run(["curl", "http://10.40.3.171:51400/?message="+message])
+            returnValue = subprocess.run(["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "curl", url])
+            print(returnValue)
+            returnValue=None
         except Exception as e:
             return "エラー：" + e
     return returnValue
