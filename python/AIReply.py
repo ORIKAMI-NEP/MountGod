67a9c2aae7e0b24e543b33d0c94906cd65bf1b62
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-13 15:10:49 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -11,7 +11,7 @@ def AIReply(message):
         try:
             # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
             AIReply = subprocess.run(
-                ["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "curl", url], encoding='utf-8', stdout=subprocess.PIPE).stdout.decode("cp932")
+                ["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "curl", url], encoding='utf-8', stdout=subprocess.PIPE).stdout #.decode("cp932")
             returnValue = ExtractJapanese(AIReply)
         except Exception as e:
             return "エラー：" + str(e)
