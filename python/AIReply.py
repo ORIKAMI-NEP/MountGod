4fd4065dfd3d5c84100b7f3f7314cbc2ecd4ceb1
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-13 20:34:50 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -1,6 +1,5 @@
 import subprocess
 import requests
-from python.ExtractJapanese import ExtractJapanese
 
 
 def AIReply(message):
@@ -8,9 +7,8 @@ def AIReply(message):
     if "\\ai " in message:
         try:
             # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
-            AIReply = subprocess.run(
+            returnValue = subprocess.run(
                 ["sshpass", "-p", "nepgear325", "ssh", "1196316@202.231.44.104", "python", "accessAPI.py", message.replace("\\ai ", "")], encoding="utf-8", stdout=subprocess.PIPE).stdout
-            returnValue = ExtractJapanese(AIReply)
         except Exception as e:
-            return "エラー：" + str(e)
+            returnValue = "エラー：" + str(e)
     return returnValue
