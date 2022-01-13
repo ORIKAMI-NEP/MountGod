1cbf232a4e57547300685484f301ef40d38189a4
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-01-13 12:12:40 +0900

    Update

--- AIReplyAPI.py
+++ AIReplyAPI.py
@@ -16,7 +16,7 @@ def AIReplyAPI():
         message = message.replace("\\naroNovel ", "")
     input_token = tokenizer.encode(message, return_tensors="pt")
     result = model.generate(input_token, do_sample=True, max_length=20,
-                            num_return_sequences=1, pad_token_id=tokenizer.eos_token_id, max_time=1.0)
+                            num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
     output = tokenizer.batch_decode(result)[0]
     pattern = re.compile(message.replace("?", "\?"))
     output = pattern.sub("", output)
