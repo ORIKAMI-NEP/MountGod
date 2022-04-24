92bc6fd85bcd62bb7b139a52e962def50d50144f
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-04-24 18:11:46 +0900

    Update

--- python/Speak.py
+++ python/Speak.py
@@ -4,7 +4,7 @@ import requests
 def Speak(message):
     returnValue = None
     try:
-        with open("./message.wav", "wb") as messageData:
+        with open("message.wav", "wb") as messageData:
             messageData.write(requests.get(
                 "http://localhost:51401/?message="+message).content)
         returnValue = "Success"
