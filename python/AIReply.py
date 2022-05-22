cff2a17f8f55d5cca711f1b27a382b4f8ccc1f71
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-05-22 20:33:00 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -10,7 +10,7 @@ def AIReply(message):
             # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
             with open("json/password.json", "r", encoding="utf-8") as Password:
                 returnValue = subprocess.run(
-                    ["sshpass", "-p", json.load(Password)["password"], "ssh", "1196316@202.231.44.104", "python", "accessAPI.py", message.replace("\\ai ", "")], encoding="utf-8", stdout=subprocess.PIPE).stdout
+                    ["sshpass", "-p", json.load(Password)["password"], "ssh", "1196316@202.231.44.104", "python", "AccessAPI.py", message.replace("\\ai ", "")], encoding="utf-8", stdout=subprocess.PIPE).stdout
         except Exception as e:
             returnValue = "エラー：" + str(e)
     return returnValue
--- python/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-message = "こんにちは"
-with open("./message.wav", "wb") as messageData:
-    messageData.write(requests.get(
-        "http://localhost:51401/?message="+message).content)
-returnValue = "Success"
