0cf469581dba16440b462fa990a62b8b24f8bc21
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-07-15 16:06:20 +0900

    Update

--- python/AIReply.py
+++ python/AIReply.py
@@ -1,4 +1,3 @@
-import requests
 import subprocess
 import json
 
@@ -7,7 +6,6 @@ def AIReply(message):
     returnValue = None
     if "\\ai " in message:
         try:
-            # returnValue = requests.get("http://10.40.3.171:51400/?message="+message).json()
             with open("json/password.json", "r", encoding="utf-8") as Password:
                 returnValue = subprocess.run(
                     ["sshpass", "-p", json.load(Password)["password"], "ssh", "1196316@202.231.44.104", "python", "AccessAPI.py", message.replace("\\ai ", "")], encoding="utf-8", stdout=subprocess.PIPE).stdout
