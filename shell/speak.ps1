65d2d6cab4cbf03adca95161421ee4ecca5f352b
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-17 08:42:59 +0900

    不要なコメント削除

--- shell/speak.ps1
+++ shell/speak.ps1
@@ -1,3 +1,2 @@
-# .\shell\speak.ps1
 Start-Process C:\VOICEVOX\run -WindowStyle Minimized
 Start-Process python MountGod.py -Verb runas -WindowStyle Minimized
--- shell/ssh.sh
+++ shell/ssh.sh
@@ -1,2 +1,1 @@
-# sh shell/ssh.sh
 ssh -o ProxyCommand='connect -H proxy.anan-nct.ac.jp:8080 %h %p' orikami@10.40.3.171
